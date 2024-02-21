2071aaab115ec9ce9381777dbe533b1327098cb5
Frederic Mencier <fmencier@yahoo.fr>
2024-02-21 22:14:12 +0100

    work

--- src/finance/Financial.java
+++ src/finance/Financial.java
@@ -11,7 +11,7 @@ import java.util.Map;
  */
 public class Financial implements FinancialInterface {
 
-    private Map<String, Double> mapCurrenciesRate;
+    private final Map<String, Double> mapCurrenciesRate;
 
     public Financial() {
         mapCurrenciesRate = Map.of("CHFEUR", 1.06D, "CHFUSD", 1.14D);
